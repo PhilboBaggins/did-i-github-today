a1ffd7b1e2be579772c20b61299a284680fa5644
Phil <PhilboBaggins@users.noreply.github.com>
2017-09-21 18:48:57 +1000

    Command line arguements with the Clap library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("bakt")
+        .version("1.0")
+        .about("An app to tell you if you've done anything on Github today")
+        .author("Phil B.")
+        .arg(Arg::with_name("username")
+            .help("Your Github username")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("verbose")
+            .short("v")
+            .long("verbose")
+            .multiple(true)
+            .help("Enable verbose output"))
+        .get_matches();
+
+    let username = matches.value_of("username").unwrap();
+    let verbose = matches.occurrences_of("verbose");
+    println!("username = {}", username);
+    println!("verbose  = {}", verbose);
 }
