1f952c312dccd85d31319d846b4c47e957922f29
Phil <PhilboBaggins@users.noreply.github.com>
2017-09-22 13:30:36 +1000

    Parse JSON, filter events and print results

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate clap;
 extern crate reqwest;
+extern crate json;
+extern crate chrono;
 
 use clap::{App, Arg};
 use std::io::Read;
+use chrono::prelude::*;
 
 fn main() {
     let matches = App::new("bakt")
@@ -32,5 +35,39 @@ fn main() {
     if let Err(error) = resp.read_to_string(&mut content) {
         panic!("{:?}", error); // TODO: Handle this better
     }
-    println!("{}", content);
+
+    let today = Local::now();
+    let parsed = json::parse(&content).unwrap(); // TODO: Don't unwrap here!!
+    if let json::JsonValue::Array(data) = parsed {
+
+        let todays_events = data.iter().filter(|x| {
+            let dt_str = x["created_at"].as_str().unwrap();
+            let dt = DateTime::parse_from_rfc3339(dt_str).unwrap(); // TODO: Is the the right date time standard
+            let dt_local = dt.with_timezone(&Local);
+            let is_today = dt_local.year() == today.year()
+                        && dt_local.month() == today.month()
+                        && dt_local.day() == today.day();
+            if is_today && verbose > 0 {
+                let event_type = x["type"].as_str().unwrap();
+                println!("{} at {}", event_type, dt_local.format("%H:%M:%S").to_string());
+            }
+            is_today
+        });
+
+        // In addition to it's obvious purpuse, I'm using the count method call
+        // below to cause side-effects in the filter above (print out today's
+        // events when verbose > 0) ... that doesn't seem so nice but it does
+        // save me from having to parse the datetime and convert it to local
+        // time again. Perhaps a filter_map would be better ... that could
+        // filter the events, returning today's events in local time along with
+        // the event type, ready to be printed when verbose > 0. Then hopefully
+        // I can find some way to short circuit evaluation of the filter_map
+        // in cases where verbose < 1.
+        if todays_events.count() > 0 {
+            println!("Yes");
+        }
+        else {
+            println!("No");
+        }
+    }
 }
