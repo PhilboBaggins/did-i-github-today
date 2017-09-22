2b97088ac4c2080b55f38143fa3dc1539b176533
Phil <PhilboBaggins@users.noreply.github.com>
2017-09-22 13:27:47 +1000

    Download events from Github's web API using reqwest library

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate clap;
+extern crate reqwest;
 
 use clap::{App, Arg};
+use std::io::Read;
 
 fn main() {
     let matches = App::new("bakt")
@@ -20,6 +22,15 @@ fn main() {
 
     let username = matches.value_of("username").unwrap();
     let verbose = matches.occurrences_of("verbose");
-    println!("username = {}", username);
-    println!("verbose  = {}", verbose);
+    let url = format!("https://api.github.com/users/{}/events", username);
+
+    // TODO: Set user-agent header
+    let mut resp = reqwest::get(&url).unwrap(); // TODO: Don't unwrap here!!
+    assert!(resp.status().is_success());  // TODO: Don't assert here
+
+    let mut content = String::new();
+    if let Err(error) = resp.read_to_string(&mut content) {
+        panic!("{:?}", error); // TODO: Handle this better
+    }
+    println!("{}", content);
 }
