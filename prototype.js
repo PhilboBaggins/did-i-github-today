7a9f56c49b95a597153f52f44a32567f88e41f7d
Phil <PhilboBaggins@users.noreply.github.com>
2017-09-27 17:43:46 +1000

    Make prototype.js print out verbose messages as well

--- prototype.js
+++ prototype.js
@@ -5,15 +5,13 @@
 const request = require('request'); // https://github.com/request/request
 const _ = require('underscore'); // http://underscorejs.org/
 
-function getDates(obj) {
-    return new Date(obj.created_at);
-}
+const verbose = ((process.argv.indexOf('-v') > 0) || (process.argv.indexOf('--verbose') > 0));
 
-function isToday(date) {
+function isToday(obj) {
     const today = new Date();
-    return date.getYear() === today.getYear() &&
-        date.getMonth() === today.getMonth() &&
-        date.getDate() === today.getDate();
+    return (new Date(obj.created_at)).getYear() === today.getYear() &&
+        (new Date(obj.created_at)).getMonth() === today.getMonth() &&
+        (new Date(obj.created_at)).getDate() === today.getDate();
 }
 
 const USER_NAME = 'philbobaggins'; // TODO: Get from command line arguments
@@ -27,8 +25,13 @@ request(options, function(error, response, body) {
     // TODO: Check response.statusCode and error variables
 
     const events = JSON.parse(body);
-    const dates = _.map(events, getDates);
-    const todaysEvents = _.filter(dates, isToday);
+    const todaysEvents = _.filter(events, isToday);
+
+    if (verbose) {
+        for (var i = 0; i < todaysEvents.length; i++) {
+            console.log(todaysEvents[i].type +' at ' +  todaysEvents[i].created_at);
+        }
+    }
 
     if (_.any(todaysEvents)) {
         console.log('Yes');
